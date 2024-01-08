afa999ad408a2ed7ffe897712dda8eecf6fa4a6d
Sattish Shewkani <spshewkani@gmail.com>
2024-01-08 23:06:03 +0800

    Update gpt.js

--- gpt.js
+++ gpt.js
@@ -1,9 +1,8 @@
-const { Configuration, OpenAIApi } = require("openai");
+const { OpenAIApi } = require("openai");
 
-const configuration = new Configuration({
-  apiKey: process.env.OPENAI_API_KEY,
+const openai = new OpenAIApi({
+  apiKey: "",
 });
-const openai = new OpenAIApi(configuration);
 
 async function generateText(prompt) {
   try {
