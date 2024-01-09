5deab2f615e483a1690f7126eaf4c8fbcae8b58c
Sattish Shewkani <spshewkani@gmail.com>
2024-01-10 00:14:55 +0800

    sign up complete

--- src/js/server.js
+++ src/js/server.js
@@ -1,36 +1,42 @@
-const express = require('express');
-const cors = require('cors');
-const { initializeApp } = require("firebase-admin/app");
-const { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } = require("firebase-admin/auth");
-initializeApp();
+const admin = require('firebase-admin')
+const { getAuth } = require("firebase-admin/auth");
+const express = require('express')
+const cors = require('cors')
+const serviceAccount = require("../../koursehub-6ebca4ab6372.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://koursehub-default-rtdb.asia-southeast1.firebasedatabase.app"
+});
 
 const app = express();
-const port = 3000; // Replace with your desired port number
-
-app.use(cors());
+const port = 5501;
 app.use(express.json());
+app.use(cors());
+
 
 // Sign up route
 app.post('/signup', async (req, res) => {
+    const { username, email, password } = req.body;
+
+    const auth = getAuth();
+    console.log("I am receiving signup request!")
     try {
-        const { email, password } = req.body;
-        const auth = getAuth();
-        const user = await createUserWithEmailAndPassword(auth, email, password);
+        const user = await auth.createUser({
+            email: email,
+            password: password
+        })
+        .catch((error) => {
+            if (error.code === 'auth/email-already-exists') {
+                console.log('User already exists')
+            } else {
+                console.log('Error creating user:', error)
+            }
+        })
+
         res.status(200).json({ message: 'User created successfully', user });
     } catch (error) {
-        res.status(500).json({ message: 'Failed to create user', error });
-    }
-});
-
-// Sign in route
-app.post('/signin', async (req, res) => {
-    try {
-        const { email, password } = req.body;
-        const auth = getAuth();
-        const user = await signInWithEmailAndPassword(auth, email, password);
-        res.status(200).json({ message: 'User signed in successfully', user });
-    } catch (error) {
-        res.status(500).json({ message: 'Failed to sign in', error });
+        console.log("ERROR", error)
     }
 });
 
