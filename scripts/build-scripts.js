d3773e66bc49d13387d9b1b52064766bf0f4dbb7
Jay <sengkit100@gmail.com>
2024-06-19 14:25:49 +0800

    addition of three.js

--- scripts/build-scripts.js
+++ scripts/build-scripts.js
@@ -1,5 +1,23 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
 
-const renderScripts = require('./render-scripts');
+const copyFile = (src, dest) => {
+    fs.copyFile(src, dest, (err) => {
+        if (err) throw err;
+    });
+};
 
-renderScripts();
+const srcDir = path.resolve(__dirname, '../src/js');
+const destDir = path.resolve(__dirname, '../dist/js');
+
+fs.readdir(srcDir, (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+        if (file !== 'server.js') {  // Exclude server.js
+            const srcPath = path.join(srcDir, file);
+            const destPath = path.join(destDir, file);
+            copyFile(srcPath, destPath);
+        }
+    });
+});
---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+const path = require('path');
+
+module.exports = {
+    entry: './src/js/threeSetup.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist/js'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                },
+            },
+        ],
+    },
+};
