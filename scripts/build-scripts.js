ba453452f0d8d0cb2334fbcf2ca306760a100ed7
Jay <sengkit100@gmail.com>
2024-06-19 14:00:29 +0800

    addition of three.js

--- scripts/build-scripts.js
+++ scripts/build-scripts.js
@@ -1,5 +1,21 @@
-'use strict';
+const fs = require('fs');
+const path = require('path');
 
-const renderScripts = require('./render-scripts');
+const copyFile = (src, dest) => {
+    fs.copyFile(src, dest, (err) => {
+        if (err) throw err;
+    });
+};
 
-renderScripts();
+const srcDir = path.resolve(__dirname, '../src/js');
+const destDir = path.resolve(__dirname, '../dist/js');
+
+fs.readdir(srcDir, (err, files) => {
+    if (err) throw err;
+
+    files.forEach(file => {
+        const srcPath = path.join(srcDir, file);
+        const destPath = path.join(destDir, file);
+        copyFile(srcPath, destPath);
+    });
+});
--- scripts/clean.js
+++ scripts/clean.js
@@ -1,7 +1,23 @@
-const sh = require('shelljs');
-const upath = require('upath');
+const fs = require('fs');
+const path = require('path');
 
-const destPath = upath.resolve(upath.dirname(__filename), '../dist');
+const deleteFiles = (directory) => {
+    fs.readdir(directory, (err, files) => {
+        if (err) throw err;
 
-sh.rm('-rf', `${destPath}/*`)
+        for (const file of files) {
+            const filePath = path.join(directory, file);
+            fs.unlink(filePath, err => {
+                if (err) throw err;
+            });
+        }
+    });
+};
 
+const directories = [
+    path.resolve(__dirname, '../dist/js'),
+    path.resolve(__dirname, '../dist/css'),
+    path.resolve(__dirname, '../dist/assets')
+];
+
+directories.forEach(deleteFiles);
