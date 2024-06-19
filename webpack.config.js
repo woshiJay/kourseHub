8e71cfc6c38e4d1b8c059c87410b7652a9283b38
Jay <sengkit100@gmail.com>
2024-06-19 14:11:00 +0800

    Revert "updates on configs"
    This reverts commit 35d825af5c2de41b706b5431cb7dc5663e413e49.

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-    entry: './dist/js/threeSetup.js',
+    entry: './src/js/threeSetup.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist/js'),
